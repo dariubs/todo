6acdf7d2fa6ea4ab6eed22ef8dbd57539dd6928b
Dariush Abbasi <dariushem@yahoo.com>
2020-06-10 10:54:06 +0430

    better ui for commandline outputs

--- go.mod
+++ go.mod
@@ -5,6 +5,10 @@ go 1.14
 require (
 	github.com/BurntSushi/toml v0.3.1
 	github.com/dariubs/s2i v0.0.0-20191211080032-ab3f7e16de18
+	github.com/go-openapi/strfmt v0.19.5 // indirect
+	github.com/jedib0t/go-pretty v4.3.0+incompatible
+	github.com/manifoldco/promptui v0.7.0
+	github.com/mattn/go-runewidth v0.0.9 // indirect
 	github.com/mattn/go-sqlite3 v2.0.3+incompatible
 	github.com/urfave/cli/v2 v2.2.0
 )
--- ops.go
+++ ops.go
@@ -6,20 +6,35 @@ import (
 	"os"
 	"strings"
 
-	"github.com/urfave/cli/v2"
 	"github.com/dariubs/s2i"
+	"github.com/jedib0t/go-pretty/table"
+	"github.com/manifoldco/promptui"
+	"github.com/urfave/cli/v2"
 )
 
 // Add a todo item
 func Add(c *cli.Context) error {
 	td := Todo{}
-
 	reader := bufio.NewReader(os.Stdin)
 	fmt.Print("todo: ")
 	td.Task, _ = reader.ReadString('\n')
 	td.Task = strings.Replace(td.Task, "\n", "", -1)
 	td.Status = "active"
+
+	prompt := promptui.Select{
+		Label: "Select Status",
+		Items: []string{"active", "doing", "done", "archived"},
+	}
+
+	_, td.Status, err = prompt.Run()
+	if err != nil {
+		fmt.Printf("Prompt failed %v\n", err)
+		return err
+	}
+
+	fmt.Printf("Task %s with status %q added :)\n", td.Task, td.Status)
 	td.Add()
+
 	return nil
 }
 
@@ -30,31 +45,49 @@ func List(c *cli.Context) error {
 	if err != nil {
 		return err
 	}
-	fmt.Printf("ID\tTask\t\tStatus\n")
-	fmt.Printf("---\t---\t\t---\n")
+	data := []table.Row{}
 	for _, v := range tds {
-		fmt.Printf("%d\t%s\t\t%s\n", v.ID, v.Task, v.Status)
+		x := table.Row{v.ID, v.Task, v.Status}
+		data = append(data, x)
 	}
+
+	t := table.NewWriter()
+	t.SetOutputMirror(os.Stdout)
+	t.AppendHeader(table.Row{"#", "Task", "Status"})
+	t.AppendRows(data)
+	t.Render()
 	return nil
 }
 
 // Doing a task
 func Doing(c *cli.Context) error {
 	td := Todo{ID: s2i.ParseUint(c.Args().Get(0), 0)}
-	td.ChangeStatus("doing")
+	err := td.ChangeStatus("doing")
+	if err != nil {
+		return err
+	}
+	fmt.Printf("Task #%d changed to doing\n", td.ID)
 	return nil
 }
 
 // Done a task
 func Done(c *cli.Context) error {
 	td := Todo{ID: s2i.ParseUint(c.Args().Get(0), 0)}
-	td.ChangeStatus("done")
+	err := td.ChangeStatus("done")
+	if err != nil {
+		return err
+	}
+	fmt.Printf("Task #%d changed to done\n", td.ID)
 	return nil
 }
 
 // Archive a task
 func Archive(c *cli.Context) error {
 	td := Todo{ID: s2i.ParseUint(c.Args().Get(0), 0)}
-	td.ChangeStatus("archived")
+	err := td.ChangeStatus("archived")
+	if err != nil {
+		return err
+	}
+	fmt.Printf("Task #%d archived\n", td.ID)
 	return nil
 }
